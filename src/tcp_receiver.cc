a1114a065fb3e0f87a86bb344d64bd9af676bc78
dustinburda <dustin99wii@gmail.com>
2023-10-02 15:55:03 -0400

    All tests pass Reciever

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -18,7 +18,7 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
         uint64_t stream_index = seqno.unwrap(isn_, inbound_stream.bytes_pushed()) - 1;
 
         bool is_last_substring = message.FIN;
-        b_fin_recv = message.FIN;
+        b_fin_recv |= message.FIN;
         Buffer payload = message.payload;
 
         reassembler.insert(stream_index, static_cast<std::string&>(payload), is_last_substring, inbound_stream);
@@ -45,7 +45,7 @@ TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   TCPReceiverMessage response;
 
-  response.ackno = (b_syn_recv_) ? std::optional<Wrap32>(Wrap32::wrap(inbound_stream.bytes_pushed() + 1 + b_fin_recv, isn_)) : nullopt;
+  response.ackno = (b_syn_recv_) ? std::optional<Wrap32>(Wrap32::wrap(inbound_stream.bytes_pushed() + 1 + (b_fin_recv && inbound_stream.is_closed()), isn_)) : nullopt;
   response.window_size = std::min(inbound_stream.available_capacity(), static_cast<uint64_t>(UINT16_MAX));
 
   return response;
